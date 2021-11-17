9609f64dc12d0faf4d0e81664646ddecd8ad579c
SkylerAuraArena <skylerauraarena@gmail.com>
2021-11-18 00:35:30 +0100

    Fin de création du bouton inutile

---
+++ src/components/ButtonUseless.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+const Button = (props) => {
+
+    const [txtBtn, setTxtBtn] = useState(props.children)
+    const [colorBtn, setColorBtn] = useState("SecondColor")
+    const [transitionColor, setTransitionColor] = useState("bg-yellow-400")
+
+    const handleOver = () => {
+        setTxtBtn("Attention, ce bouton est inutile !")
+    }
+
+    const handleExit = () => {
+        setTxtBtn(props.children)
+        setColorBtn("SecondColor")
+        setTransitionColor("bg-yellow-400")
+    }
+
+    const handleClick = () => {
+        setTxtBtn("Je vous l'avais bien dit !")
+        setTransitionColor("bg-red-600")
+    }
+
+    // transition duration-50 ease-in-out
+    const css = `${colorBtn}
+                rounded-3xl border-4 p-2
+                hover:${transitionColor} transform hover:scale-110`
+
+    return(
+        <span className={css} onMouseOver={()=>handleOver()} onMouseLeave={()=>handleExit()} onClick={()=>handleClick()}>{txtBtn}</span>
+    )
+}
+
+export default Button
