8d616c145e23911df153efcd62779af4c192b735
SkylerAuraArena <skylerauraarena@gmail.com>
2021-11-17 23:55:03 +0100

    Ajout de la couleur secondaire
    Création du composant bouton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Button from "./components/Button";
+
 function App() {
   return (
-    <div className="App box-border m-0 text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center text-white text-3xl hover:bg-red-700">
-        Un site
+    <div className="MainColor box-border m-0 text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center text-white text-3xl">
+        <Button>Un bouton</Button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// hover:bg-red-700
